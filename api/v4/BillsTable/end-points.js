6f3d83a473349cc2f4a208b9b5fd86e1f7003c29
keshavsoft <nknnkumar@yahoo.com>
2026-07-02 10:03:54 +0530

    api4

--- api/v4/BillsTable/end-points.js
+++ api/v4/BillsTable/end-points.js
@@ -1,9 +1,15 @@
 import express from 'express';
 
+import funcFrominsertWithMeta from './insertWithMeta/controller.js';
+import funcFromshowAll from './showAll/controller.js';
+
 const tableName = "BillsTable.json";
 const tablePath = "Data/BillsTable.json";
 const configPath = "Config/Schemas/BillsTable.json";
 
 const router = express.Router();
+
+router.post('/insertWithMeta', express.json(), (req, res) => funcFrominsertWithMeta({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.get('/showAll', (req, res) => funcFromshowAll({ req, res, inTablePath: tablePath }));
 
 export { router };
