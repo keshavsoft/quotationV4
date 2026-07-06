41e64e3c30ddf9a1ec6b2f9257ff59eb0ea6f74f
keshavsoft <nknnkumar@yahoo.com>
2026-07-06 22:30:27 +0530

    del

--- api/v6/BillsTable/end-points.js
+++ api/v6/BillsTable/end-points.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import funcFromfind from './find/controller.js';
 import funcFrommodify from './modify/controller.js';
+import funcFromdel from './del/controller.js';
 
 const tableName = "BillsTable.json";
 const tablePath = "Data/BillsTable.json";
@@ -11,5 +12,6 @@ const router = express.Router();
 
 router.get('/find/:pk', (req, res) => funcFromfind({ req, res, inTablePath: tablePath }));
 router.put('/modify', express.json(), (req, res) => funcFrommodify({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.delete('/del/:pk', (req, res) => funcFromdel({ req, res, inTablePath: tablePath }));
 
 export { router };
