d3f9553488736ca18788804c14bd10f1e521cfac
keshavsoft <nknnkumar@yahoo.com>
2026-07-06 22:31:12 +0530

    post

--- api/v6/BillsTable/end-points.js
+++ api/v6/BillsTable/end-points.js
@@ -3,6 +3,7 @@ import express from 'express';
 import funcFromfind from './find/controller.js';
 import funcFrommodify from './modify/controller.js';
 import funcFromdel from './del/controller.js';
+import funcFrominsertWithMeta from './insertWithMeta/controller.js';
 
 const tableName = "BillsTable.json";
 const tablePath = "Data/BillsTable.json";
@@ -13,5 +14,6 @@ const router = express.Router();
 router.get('/find/:pk', (req, res) => funcFromfind({ req, res, inTablePath: tablePath }));
 router.put('/modify', express.json(), (req, res) => funcFrommodify({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 router.delete('/del/:pk', (req, res) => funcFromdel({ req, res, inTablePath: tablePath }));
+router.post('/insertWithMeta', express.json(), (req, res) => funcFrominsertWithMeta({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 
 export { router };
